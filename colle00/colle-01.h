15a37f265c76c1aa5b6f327e81dc87138e28f82d
Chr0nos <hisoka2501@free.fr>
2015-03-10 13:18:06 +0100

    updated the header for colle01

--- colle00/colle-01.h
+++ colle00/colle-01.h
@@ -1,6 +1,8 @@
-#ifndef FT_COLLE00_H
-#define FT_COLLE00_H
+#ifndef FT_COLLE01_H
+#define FT_COLLE01_H
 
+void colle_footer(int line_num, int *lenght, int *i);
+void colle_header(int line_num, int *lenght, int *i);
 void colle_line(int line_num, int lenght);
 void colle(int x, int y);
 
